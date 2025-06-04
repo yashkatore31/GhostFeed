a8de7d0cc7173c8688c5a09d61847fd8583a4a9a
Devyash <ybkatore31@gmail.com>
2025-06-04 16:31:15 +0530

    some bug fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,54 @@
 import type { Metadata } from "next";
-import {
-    ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
 import { AuthSync } from "../components/AuthSync";
 import { dark } from "@clerk/themes";
 import { UserProvider } from "@/context/UserContext";
 import { Analytics } from '@vercel/analytics/next';
+import Script from "next/script";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "GhostFeed",
-    description: "Created by Yash Katore",
+  title: "GhostFeed",
+  description: "Created by Yash Katore",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <ClerkProvider
-            appearance={{
-                baseTheme: dark,
-                variables: {
-                    colorPrimary: "#ff4500",
-                },
-            }}
-        >
-            <html lang="en">
-                <head>
-                    <link rel="icon" href="/ghost.ico" sizes="any" />
-                </head>
-                <body
-                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                >
-                    <header className="flex justify-end items-center p-4 gap-4 h-16 right-0 fixed">
-                    </header>
-                    <UserProvider>
-                        <AuthSync>{children}</AuthSync>
-                        <Analytics />
-                    </UserProvider>
-                </body>
-            </html>
-        </ClerkProvider>
-    );
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#ff4500",
+        },
+      }}
+    >
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/ghost.ico" sizes="any" />
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16 right-0 fixed"></header>
+          <UserProvider>
+            <AuthSync>{children}</AuthSync>
+            <Analytics />
+          </UserProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
